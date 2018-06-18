6c9b6514ed8459098de4f0955fa7af954d6641d9
walid <bousnina.walid1@gmail.com>
2018-06-18 14:37:58 +0200

    Initial commit

---
+++ src/AppBundle/Twig/Extension/home.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class home extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'home';
+    }
+}
